aaa0d22c1bc80c287e408dcadfb254eed04ab75c
Michał Wójcik <01153102@pw.edu.pl>
2022-06-22 00:39:46 +0200

    Deleted unnecessary files, theme and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import Layout from "./components/layout/Layout";
 import MainPage from "./pages/MainPage";
 import LayoutAuthenticated from './components/layout/LayoutAuthenticated';
+import {darkTheme} from "./theme";
 
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-    primary :{
-      main : "#007AFF"
-    }
-
-
-
-  },
-});
 
 function App() {
   return (
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+export const darkTheme = createTheme({
+  typography: {
+    fontFamily: ["Roboto", "sans-serif"].join(","),
+  },
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#007AFF",
+    },
+  },
+});
