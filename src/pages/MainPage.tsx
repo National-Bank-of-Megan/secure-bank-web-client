a800a6b234e3625a8091d9d58f80efe92d14ec7a
kasiagrygorowicz <kasiagrygorowicz@interia.eu>
2022-06-21 18:03:06 +0200

    welcome page for unauthenticated user

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,14 @@
+import { Grid } from "@mui/material";
 import WelcomeCard from "../components/main-page/WelcomeCard";
 
-export default function MainPage(){
-
-    return (
-
-        <WelcomeCard/>
-    )
+export default function MainPage() {
+  return (
+    <Grid container
+      style={{
+        height: "100vh"
+      }}
+    >
+      <WelcomeCard/>
+    </Grid>
+  );
 }
